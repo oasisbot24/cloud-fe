820f6a965e3dae66499917e60fdbae75581fae10
DeveloperRyou <DeveloperRyou@gmail.com>
2024-01-29 17:07:59 +0900

    FEAT: Google Oauth 로그인 구현

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,15 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  console.log(process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID);
+  return (
+    <GoogleOAuthProvider
+      clientId={process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID ?? ""}
+    >
+      <Component {...pageProps} />
+    </GoogleOAuthProvider>
+  );
 }
