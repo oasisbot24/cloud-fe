38e718898ad58506dd39edade702a9ff3f01b646
DeveloperRyou <DeveloperRyou@gmail.com>
2024-02-08 00:08:17 +0900

    FIX: footer layout 수정

--- src/components/TopNavLayout.tsx
+++ src/components/TopNavLayout.tsx
@@ -4,11 +4,10 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import { Button, Paper, Stack } from "@mui/material";
 import { useAtom } from "jotai";
-import authAtom from "@/datas/auth";
-import { Paper, Stack } from "@mui/material";
 
 /* Components */
 import Footer from "@/components/basic/Footer";
+import authAtom from "@/datas/auth";
 
 /* interfaces */
 interface TopNavLayoutProps {
@@ -102,7 +101,14 @@ export default function TopNavLayout({ children }: TopNavLayoutProps) {
           className="min-h-[calc(100%-60px)] h-[calc(100%-60px)] px-4 gap-[20px]"
         >
           <div className="w-1/12" />
-          <div className="w-5/6 py-6 z-10">{children}</div>
+          <Stack
+            direction="column"
+            justifyContent="space-between"
+            className="w-5/6 py-6 z-10"
+          >
+            {children}
+            <Footer center />
+          </Stack>
           <div className="w-1/12" />
         </Stack>
         <Image
@@ -112,7 +118,6 @@ export default function TopNavLayout({ children }: TopNavLayoutProps) {
           className="w-full h-auto fixed bottom-0 z-0"
           alt="bg"
         />
-        <Footer center />
       </Stack>
     </main>
   );
