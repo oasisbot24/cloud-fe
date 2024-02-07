1efff83b0247f2a7a167cdb21428fa8cabceb0b3
DeveloperRyou <DeveloperRyou@gmail.com>
2024-02-07 18:11:44 +0900

    FIX: 첫 화면 접근시 로그인 검사

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -6,11 +8,23 @@ import "@fontsource/roboto/700.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useAtom } from "jotai";
+import { jwtDecode } from "jwt-decode";
+import authAtom from "@/datas/auth";
 import "@/styles/globals.css";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [, setAuth] = useAtom(authAtom);
+
+  useEffect(() => {
+    const credential = localStorage.getItem("credential");
+    if (credential) setAuth(jwtDecode(credential));
+    else router.push("/");
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
