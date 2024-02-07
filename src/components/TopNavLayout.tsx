d4c8d72af9cf800e5b571920ada19f3f1db8da75
DeveloperRyou <DeveloperRyou@gmail.com>
2024-02-07 18:12:24 +0900

    FIX: profile 사진 google과 동기화

--- src/components/TopNavLayout.tsx
+++ src/components/TopNavLayout.tsx
@@ -1,12 +1,41 @@
 import React from "react";
-import { Paper, Stack } from "@mui/material";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { Button, Paper, Stack } from "@mui/material";
+import { useAtom } from "jotai";
+import authAtom from "@/datas/auth";
 
 interface TopNavLayoutProps {
   children: React.ReactNode;
 }
 
+const route = [
+  {
+    name: "대시보드",
+    path: "/main",
+  },
+  {
+    name: "오아시스 봇",
+    path: "/bot",
+  },
+  {
+    name: "백테스트",
+    path: "/backtest",
+  },
+  {
+    name: "프리셋 연결",
+    path: "/preset",
+  },
+  {
+    name: "API 연결",
+    path: "/api",
+  },
+];
+
 export default function TopNavLayout({ children }: TopNavLayoutProps) {
+  const [auth] = useAtom(authAtom);
+  const router = useRouter();
+  const { pathname } = router;
   return (
     <main className="w-full h-screen">
       <Stack className="h-full">
@@ -30,11 +59,46 @@ export default function TopNavLayout({ children }: TopNavLayoutProps) {
                 height={60}
               />
             </div>
-            <div className="w-5/6">Logo</div>
-            <div className="w-1/12">Nav</div>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+              className="w-5/6 gap-[20px]"
+            >
+              {route.map(item => (
+                <Button
+                  key={item.name}
+                  variant={pathname === item.path ? "contained" : "text"}
+                  className={`w-1/5 ${
+                    pathname === item.path ? "bg-white text-black" : ""
+                  }`}
+                  color="inherit"
+                  onClick={() => router.push(item.path)}
+                >
+                  {item.name}
+                </Button>
+              ))}
+            </Stack>
+            <Stack className="w-1/12 items-center">
+              <Image
+                src={auth?.picture}
+                alt="profile"
+                width={40}
+                height={40}
+                className="rounded-full"
+              />
+            </Stack>
           </Stack>
         </Paper>
-        <div className="px-16 py-8 h-full">{children}</div>
+        <Stack
+          direction="row"
+          justifyContent="between"
+          className="min-h-[calc(100%-60px)] h-[calc(100%-60px)] px-4 gap-[20px]"
+        >
+          <div className="w-1/12" />
+          <div className="w-5/6 py-6">{children}</div>
+          <div className="w-1/12" />
+        </Stack>
       </Stack>
     </main>
   );
