559b40a62c516c7b4e0b691b5ec3e2be289ef86d
DeveloperRyou <DeveloperRyou@gmail.com>
2024-02-18 20:29:29 +0900

    FIX: Profile 눌렀을 때 추가 메뉴 보이게 수정

--- src/components/TopNavLayout.tsx
+++ src/components/TopNavLayout.tsx
@@ -1,8 +1,19 @@
 /* Dependencies */
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { Button, Paper, Stack } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  MenuItem,
+  MenuList,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { useAtom } from "jotai";
 
 /* Components */
@@ -39,86 +50,129 @@ const route = [
 
 export default function TopNavLayout({ children }: TopNavLayoutProps) {
   const [auth] = useAtom(authAtom);
+  const [isOpened, setIsOpened] = useState(false);
+
+  const openMenu = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setIsOpened(true);
+  };
+
+  const closeMenu = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setIsOpened(false);
+  };
+
   const router = useRouter();
   const { pathname } = router;
   return (
-    <main className="w-full h-screen bg-gray-80">
-      <Stack className="h-full">
-        <Paper
-          variant="elevation"
-          square
-          elevation={4}
-          className="bg-darkBlue h-[60px]"
-        >
-          <Stack
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-            className="h-full px-4 text-center text-white gap-[20px]"
-          >
-            <div className="w-1/12 h-full">
-              <Image
-                src="/logo/oasis-white.svg"
-                alt="logo"
-                width={60}
-                height={60}
-              />
-            </div>
+    <Box onClick={closeMenu}>
+      <main className="w-full h-screen">
+        <Stack className="h-full">
+          <AppBar position="static" className="bg-darkBlue h-[60px]">
             <Stack
               direction="row"
               justifyContent="space-between"
               alignItems="center"
-              className="w-5/6 gap-[20px]"
+              className="h-full px-4 text-center text-white gap-[20px]"
             >
-              {route.map(item => (
-                <Button
-                  key={item.name}
-                  variant={pathname === item.path ? "contained" : "text"}
-                  className={`w-1/5 ${
-                    pathname === item.path ? "bg-white text-black" : ""
-                  }`}
-                  color="inherit"
-                  onClick={() => router.push(item.path)}
-                >
-                  {item.name}
-                </Button>
-              ))}
+              <div className="w-1/12 h-full">
+                <Image
+                  src="/logo/oasis-white.svg"
+                  alt="logo"
+                  width={60}
+                  height={60}
+                />
+              </div>
+              <Stack
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+                className="w-5/6 gap-[20px]"
+              >
+                {route.map(item => (
+                  <Button
+                    key={item.name}
+                    variant={pathname === item.path ? "contained" : "text"}
+                    className={`w-1/5 ${
+                      pathname === item.path ? "bg-white text-black" : ""
+                    }`}
+                    color="inherit"
+                    onClick={() => router.push(item.path)}
+                  >
+                    {item.name}
+                  </Button>
+                ))}
+              </Stack>
+              <Stack className="w-1/12 items-center">
+                <div>
+                  <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={openMenu}
+                    color="inherit"
+                  >
+                    <Image
+                      src={auth?.picture}
+                      alt="profile"
+                      width={40}
+                      height={40}
+                      className="rounded-full"
+                    />
+                  </IconButton>
+                  {isOpened && (
+                    <Paper className="absolute top-[60px] right-0 min-w-[220px]">
+                      <MenuList>
+                        <div className="px-4 py-2 text-left">
+                          <Typography variant="body1">{auth?.name}</Typography>
+                          <Typography variant="body2" className="text-gray-500">
+                            {auth?.email}
+                          </Typography>
+                        </div>
+                        <Divider className="my-2" />
+                        <MenuItem onClick={() => {}} className="px-4">
+                          거래소 변경
+                        </MenuItem>
+                        <MenuItem onClick={() => {}} className="px-4">
+                          요금 안내
+                        </MenuItem>
+                        <Divider className="my-2" />
+                        <MenuItem onClick={() => {}} className="px-4">
+                          로그아웃
+                        </MenuItem>
+                      </MenuList>
+                    </Paper>
+                  )}
+                </div>
+              </Stack>
             </Stack>
-            <Stack className="w-1/12 items-center">
-              <Image
-                src={auth?.picture}
-                alt="profile"
-                width={40}
-                height={40}
-                className="rounded-full"
-              />
-            </Stack>
-          </Stack>
-        </Paper>
-        <Stack
-          direction="row"
-          justifyContent="between"
-          className="min-h-[calc(100%-60px)] h-[calc(100%-60px)] px-4 gap-[20px]"
-        >
-          <div className="w-1/12" />
+          </AppBar>
           <Stack
-            direction="column"
-            justifyContent="space-between"
-            className="h-full w-5/6 py-6 z-10"
+            direction="row"
+            justifyContent="between"
+            className="min-h-[calc(100%-60px)] h-[calc(100%-60px)] px-4 gap-[20px]"
           >
-            {children}
-            <Footer center />
+            <div className="w-1/12" />
+            <Stack
+              direction="column"
+              justifyContent="space-between"
+              className="w-5/6 py-6 z-10"
+            >
+              {children}
+              <Footer center />
+            </Stack>
+            <div className="w-1/12" />
           </Stack>
-          <div className="w-1/12" />
+          <Image
+            src="/main/hero-bg.png"
+            width={1800}
+            height={234}
+            className="w-full h-auto fixed bottom-0 z-0"
+            alt="bg"
+          />
         </Stack>
-        <Image
-          src="/main/hero-bg.png"
-          width={1800}
-          height={234}
-          className="w-full h-auto fixed bottom-0 z-0"
-          alt="bg"
-        />
-      </Stack>
-    </main>
+      </main>
+    </Box>
   );
 }
