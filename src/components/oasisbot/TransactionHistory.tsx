cddec0fef0fdbb6fefd1c3907946fb77177ab3fd
Heeseung Koo <nrudev812@gmail.com>
2024-02-18 20:41:06 +0900

    REFACTOR: DataGrid 일부 셀 스타일 적용

--- src/components/oasisbot/TransactionHistory.tsx
+++ src/components/oasisbot/TransactionHistory.tsx
@@ -1,17 +1,96 @@
 import React from "react";
-import { Box } from "@mui/material";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { Box, Chip, Typography } from "@mui/material";
+import { DataGrid, GridCellParams, GridColDef } from "@mui/x-data-grid";
+import { amber, purple } from "@mui/material/colors";
 
 function TransactionHistory() {
   const columns: GridColDef[] = [
-    { field: "time", headerName: "거래시각", width: 220 },
-    { field: "item", headerName: "종목", width: 220 },
-    { field: "position", headerName: "포지션", width: 220 },
-    { field: "price", headerName: "매수/매도가", width: 220 },
-    { field: "amount", headerName: "매수/매도수량", width: 220 },
-    { field: "totalPrice", headerName: "총매수/매도금액", width: 220 },
-    { field: "profitRatio", headerName: "손익률", width: 220 },
-    { field: "realizedProfit", headerName: "실현손익", width: 220 },
+    { field: "time", headerName: "거래시각", width: 170 },
+    {
+      field: "item",
+      headerName: "종목",
+      width: 150,
+      headerAlign: "center",
+      align: "center",
+    },
+    {
+      field: "position",
+      headerName: "포지션",
+      width: 150,
+      headerAlign: "center",
+      align: "center",
+      renderCell: (params: GridCellParams) => (
+        <Chip
+          label={params.value}
+          sx={{
+            userSelect: "none",
+            fontWeight: 500,
+            backgroundColor: params.value === "BUY" ? purple[100] : amber[200],
+            color: "white",
+            borderRadius: 2,
+          }}
+        />
+      ),
+    },
+    {
+      field: "price",
+      headerName: "매수/매도가",
+      width: 220,
+      headerAlign: "center",
+      align: "center",
+      renderCell: (params: GridCellParams) => (
+        <Typography variant="body2" component="span">
+          {params.value.toLocaleString() + " KRW"}
+        </Typography>
+      ),
+    },
+    {
+      field: "amount",
+      headerName: "매수/매도수량",
+      width: 150,
+      headerAlign: "center",
+      align: "center",
+    },
+    {
+      field: "totalPrice",
+      headerName: "총매수/매도금액",
+      width: 220,
+      headerAlign: "center",
+      align: "center",
+      renderCell: (params: GridCellParams) => (
+        <Typography variant="body2" component="span">
+          {params.value.toLocaleString() + " KRW"}
+        </Typography>
+      ),
+    },
+    {
+      field: "profitRatio",
+      headerName: "손익률",
+      width: 220,
+      headerAlign: "center",
+      align: "center",
+      renderCell: (params: GridCellParams) => (
+        <Typography
+          variant="body2"
+          component="span"
+          color={params.value.slice(0, 1) === "-" ? "blue" : "red"}
+        >
+          {params.value}
+        </Typography>
+      ),
+    },
+    {
+      field: "realizedProfit",
+      headerName: "실현손익",
+      width: 220,
+      headerAlign: "center",
+      align: "center",
+      renderCell: (params: GridCellParams) => (
+        <Typography variant="body2" component="span">
+          {params.value.toLocaleString() + " KRW"}
+        </Typography>
+      ),
+    },
   ];
 
   const rows = [
@@ -34,7 +113,7 @@ function TransactionHistory() {
       price: 30122334,
       amount: 0.1,
       totalPrice: 3012233,
-      profitRatio: "+0.1%",
+      profitRatio: "-0.1%",
       realizedProfit: 2122,
     },
   ];
