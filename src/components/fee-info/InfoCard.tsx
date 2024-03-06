8ee2160dc8630936779f8348878d935842374513
DeveloperRyou <DeveloperRyou@gmail.com>
2024-03-06 16:24:16 +0900

    FIX: 컴포넌트 분리

---
+++ src/components/fee-info/InfoCard.tsx
@@ -0,0 +1,71 @@
+import { Button, Card, Chip, Stack, Typography } from "@mui/material";
+
+interface InfoCardProps {
+  type: "free" | "premium" | "platunum";
+}
+
+const textColorStyle = {
+  free: "text-stone-500",
+  premium: "text-slate-700",
+  platunum: "text-blue-600",
+};
+
+const bgColorStyle = {
+  free: "bg-stone-500",
+  premium: "bg-slate-700",
+  platunum: "bg-blue-600",
+};
+
+export default function InfoCard({ type }: InfoCardProps) {
+  return (
+    <Card className="bg-white h-[450px] w-full shrink-0 lg:shrink">
+      <Stack className="box-border h-full py-8 px-4 items-center justify-between">
+        <Stack direction="column" className="gap-4 items-center">
+          <Typography
+            className={`font-roboto text-4xl font-semibold ${textColorStyle[type]}`}
+          >
+            {type.toUpperCase()}
+          </Typography>
+          <Typography className={`font-roboto text-xl ${textColorStyle[type]}`}>
+            <span className="text-4xl font-medium">0원</span> / 14일 무료
+          </Typography>
+        </Stack>
+        <Button
+          variant="contained"
+          className={`w-full p-2 ${bgColorStyle[type]}`}
+        >
+          {type === "free" ? "14일 무료체험" : "무료체험 후 결제"}
+        </Button>
+        <Typography
+          className={`text-md font-roboto font-medium ${textColorStyle[type]}`}
+        >
+          {type === "free"
+            ? "오아시스봇을 처음 이용해보시는분들께 추천합니다."
+            : "트레블룰 승인 된 해외거래소를이용하시는 분들께 추천합니다."}
+        </Typography>
+        <Stack direction="column" className="gap-2 items-center">
+          <Stack direction="row" className="gap-2 items-center">
+            <Chip
+              label="유료요금제 동일 기능"
+              className={`${bgColorStyle[type]} text-white`}
+            />
+            <Chip
+              label="24시간 작동"
+              className={`${bgColorStyle[type]} text-white`}
+            />
+          </Stack>
+          <Stack direction="row" className="gap-2 items-center">
+            <Chip
+              label="지속적인 기능 및 로직 추가"
+              className={`${bgColorStyle[type]} text-white`}
+            />
+            <Chip
+              label="모바일 확인 가능"
+              className={`${bgColorStyle[type]} text-white`}
+            />
+          </Stack>
+        </Stack>
+      </Stack>
+    </Card>
+  );
+}
--- src/pages/fee-info/index.tsx
+++ src/pages/fee-info/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import { Button, Card, Chip, Stack, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
+import InfoCard from "@/components/fee-info/InfoCard";
 import TopNavLayout from "@/components/topnav/TopNavLayout";
 
 export default function FeeInfo() {
@@ -7,7 +8,7 @@ export default function FeeInfo() {
     <TopNavLayout hasBackground={false}>
       <Stack
         direction="column"
-        className="h-full w-full items-center justify-between"
+        className="w-full items-center justify-between gap-8"
       >
         <Stack direction="column" className="gap-2 items-center">
           <Image
@@ -23,51 +24,10 @@ export default function FeeInfo() {
             클릭시 결제모듈로 이동합니다.
           </Typography>
         </Stack>
-
-        <Stack
-          direction="row"
-          className="h-full w-full justify-between items-center"
-        >
-          <Card className="bg-white h-full w-1/3">
-            <Stack className="box-border h-full py-8 px-4 items-center justify-between">
-              <Stack direction="column" className="gap-4 items-center">
-                <Typography className="font-roboto text-4xl text-stone-500 font-semibold">
-                  Free
-                </Typography>
-                <Typography className="font-roboto text-xl text-stone-500">
-                  <span className="text-4xl font-medium">0원</span> / 14일 무료
-                </Typography>
-              </Stack>
-              <Button variant="contained" className="bg-stone-500 w-full p-2">
-                14일 무료체험
-              </Button>
-              <Typography className="text-md font-roboto text-stone-500 font-medium">
-                오아시스봇을 처음 이용해보시는분들께 추천합니다.
-              </Typography>
-              <Stack direction="column" className="gap-2 items-center">
-                <Stack direction="row" className="gap-2 items-center">
-                  <Chip
-                    label="유료요금제 동일 기능"
-                    className="bg-stone-500 text-white"
-                  />
-                  <Chip
-                    label="24시간 작동"
-                    className="bg-stone-500 text-white"
-                  />
-                </Stack>
-                <Stack direction="row" className="gap-2 items-center">
-                  <Chip
-                    label="지속적인 기능 및 로직 추가"
-                    className="bg-stone-500 text-white"
-                  />
-                  <Chip
-                    label="모바일 확인 가능"
-                    className="bg-stone-500 text-white"
-                  />
-                </Stack>
-              </Stack>
-            </Stack>
-          </Card>
+        <Stack className="lg:flex-row flex-col h-full w-full justify-between items-center gap-4">
+          <InfoCard type="free" />
+          <InfoCard type="premium" />
+          <InfoCard type="platunum" />
         </Stack>
       </Stack>
     </TopNavLayout>
