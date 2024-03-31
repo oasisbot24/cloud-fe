67d218b85176dd4d9178a32c49c0a0e0d108e4ae
DeveloperRyou <DeveloperRyou@gmail.com>
2024-03-31 21:22:08 +0900

    FIX: main 디자인 에셋 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Stack } from "@mui/material";
 import Footer from "@/screens/layout/Footer";
 import Header from "@/screens/layout/Header";
@@ -8,7 +9,16 @@ export default function Home() {
     <main className="flex w-full h-screen signup-bg">
       <Stack className="w-full">
         <Header color="white" />
-        <Discription />
+        <Stack direction="row" className="w-full h-full relative">
+          <Discription />
+          <Image
+            src="/main/mobius.png"
+            alt="mobius"
+            height={1080}
+            width={1080}
+            className="absolute right-0 bottom-10 h-full w-fit object-contain z-0"
+          />
+        </Stack>
         <Footer color="white" />
       </Stack>
     </main>
