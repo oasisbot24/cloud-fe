e4f3cbbd6c683052faac45590b03532e5cfdd10b
Heeseung Koo <nrudev812@gmail.com>
2024-04-17 22:23:36 +0900

    FIX: styled 임포트 수정

--- src/components/common/IosSwitch.tsx
+++ src/components/common/IosSwitch.tsx
@@ -1,5 +1,5 @@
-import styled from "@emotion/styled";
 import { Switch, SwitchProps } from "@mui/material";
+import { styled } from "@mui/material/styles";
 
 const IosSwitch = styled((props: SwitchProps) => (
   <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
