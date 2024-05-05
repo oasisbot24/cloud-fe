a728d25641ffa6228237cb3e53ef1ab0dcb234ea
DeveloperRyou <DeveloperRyou@gmail.com>
2024-05-05 17:32:12 +0900

    FIX: CircleChartCard 에 헤더 추가

--- src/cards/CircleChartCard/index.tsx
+++ src/cards/CircleChartCard/index.tsx
@@ -1,13 +1,31 @@
 import { Box, Stack } from "@mui/material";
 import Card from "@/cards/Card";
+import CardHeader from "@/cards/CardHeader";
 import SelectButtonList from "@/cards/CircleChartCard/SelectButtonList";
+import useCurrencySelect from "@/hooks/common/useCurrencySelect";
+import useMarketSelect from "@/hooks/common/useMarketSelect";
+import usePriceStatusSelect from "@/hooks/common/usePriceStatusSelect";
 import CircleChart from "./CircleChart";
 
 export default function CircleChartCard() {
+  const { PriceStatusSelect } = usePriceStatusSelect();
+  const { CurrencySelect } = useCurrencySelect();
+  const { MarketSelect } = useMarketSelect();
+
   return (
     <Card>
-      <Box className="p-4">실시간 자산비율</Box>
-      <Stack direction="row" className="justify-center mt-[-20px]">
+      <CardHeader
+        id="pie-chart"
+        title="실시간 자산 비율"
+        action={
+          <>
+            <PriceStatusSelect />
+            <CurrencySelect />
+            <MarketSelect />
+          </>
+        }
+      />
+      <Stack direction="row" className="justify-center mt-[-40px]">
         <CircleChart />
       </Stack>
       <Box className="p-4 absolute w-full bottom-0">
