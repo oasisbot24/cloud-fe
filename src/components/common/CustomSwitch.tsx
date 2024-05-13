a365efe8f3772170ec63b5ef74e4bfec6c5d625d
DeveloperRyou <DeveloperRyou@gmail.com>
2024-05-13 17:19:01 +0900

    FEAT: 커스텀 컴포넌트 추가

---
+++ src/components/common/CustomSwitch.tsx
@@ -0,0 +1,39 @@
+import { Switch, styled } from "@mui/material";
+
+const CustomSwitch = styled(Switch)(({ theme }) => ({
+  width: 28,
+  height: 16,
+  padding: 0,
+  display: "flex",
+  "& .MuiSwitch-switchBase": {
+    padding: 2,
+    "&.Mui-checked": {
+      transform: "translateX(12px)",
+      "& + .MuiSwitch-track": {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === "dark" ? "#177ddc" : "#1890ff",
+      },
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    boxShadow: "0px 2px 4px 0px #00230B33",
+    width: 12,
+    height: 12,
+    borderRadius: 6,
+    color: "#fff",
+    transition: theme.transitions.create(["width"], {
+      duration: 200,
+    }),
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: 16 / 2,
+    opacity: 1,
+    backgroundColor:
+      theme.palette.mode === "dark"
+        ? "rgba(255,255,255,.35)"
+        : "rgba(0,0,0,.25)",
+    boxSizing: "border-box",
+  },
+}));
+
+export default CustomSwitch;
