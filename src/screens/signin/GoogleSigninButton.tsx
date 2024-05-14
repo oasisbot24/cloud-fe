355e8542eaeb55c41254fa135afad68b636d4ceb
DeveloperRyou <DeveloperRyou@gmail.com>
2024-05-14 12:36:30 +0900

    FIX: onetap login 기능 임시 추가

--- src/screens/signin/GoogleSigninButton.tsx
+++ src/screens/signin/GoogleSigninButton.tsx
@@ -1,19 +1,50 @@
 import { ButtonBase, Typography } from "@mui/material";
-import { useGoogleLogin } from "@react-oauth/google";
+import {
+  CredentialResponse,
+  useGoogleLogin,
+  useGoogleOneTapLogin,
+} from "@react-oauth/google";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useAtom } from "jotai";
+import { jwtDecode } from "jwt-decode";
+import signin from "@/apis/signin";
 import Icon from "@/components/Icon/index";
+import authAtom, { Auth } from "@/datas/auth";
 
 function GoogleSigninButton({ onClick }: { onClick?: () => void }) {
-  const signIn = useGoogleLogin({
-    flow: "auth-code",
-    onSuccess: () => {
-      console.log("Login Success");
-      onClick?.();
-    },
+  const [, setAuth] = useAtom(authAtom);
+  const signinMutation = useMutation({
+    mutationFn: signin,
+  });
+  const onSuccess = (credentialResponse: CredentialResponse) => {
+    console.log(credentialResponse);
+    const { credential } = credentialResponse;
+    signinMutation.mutate(credential || "", {
+      onSuccess: () => {
+        localStorage.setItem("credential", credential || "");
+        const auth = jwtDecode(credential || "") as Auth;
+        setAuth(auth);
+      },
+    });
+    onClick?.();
+  };
+
+  useGoogleOneTapLogin({
+    onSuccess,
   });
 
-  const handleClick = () => {
-    signIn();
-  };
+  const handleClick = useGoogleLogin({
+    onSuccess: async tokenResponse => {
+      console.log(tokenResponse);
+      const userInfo = await axios
+        .get("https://www.googleapis.com/oauth2/v3/userinfo", {
+          headers: { Authorization: `Bearer ${tokenResponse.access_token}` },
+        })
+        .then(res => res.data);
+      console.log(userInfo);
+    },
+  });
 
   return (
     <ButtonBase
@@ -21,7 +52,7 @@ function GoogleSigninButton({ onClick }: { onClick?: () => void }) {
       sx={{
         border: "1px solid",
       }}
-      onClick={handleClick}
+      onClick={() => handleClick()}
     >
       <Icon src="/google.png" size={20} />
       <Typography variant="bodyS">Google로 계속하기</Typography>
