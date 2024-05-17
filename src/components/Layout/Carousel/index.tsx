cf83830411aa77871f0cacf78b6d50259f43d158
DeveloperRyou <DeveloperRyou@gmail.com>
2024-05-17 19:26:50 +0900

    FEAT: Carousel 기능 추가

---
+++ src/components/Layout/Carousel/index.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
+import { Box, IconButton } from "@mui/material";
+import useComponentSize from "@/hooks/useComponentSize";
+
+interface CarouselProps {
+  children: React.ReactNode;
+  minWidth: number;
+}
+const buttonStyle =
+  "fixed rounded-full bg-[#FFFFFF9D] top-1/2 -translate-y-1/2 z-10 w-[44px] h-[44px] border border-solid border-black shadow-md";
+
+export default function Carousel({ children, minWidth }: CarouselProps) {
+  const { size, componentRef } = useComponentSize();
+  const [align, setAlign] = useState<"left" | "right">("left");
+  return (
+    <Box ref={componentRef} className="w-full">
+      <Box className="relative w-full" sx={{ minWidth: `${minWidth}px` }}>
+        {size.width <= minWidth && align === "right" && (
+          <IconButton
+            className={`left-[calc(80px + 4rem)] ${buttonStyle}`}
+            onClick={() => setAlign("left")}
+          >
+            <KeyboardArrowLeft fontSize="medium" />
+          </IconButton>
+        )}
+        <Box
+          className="absolute w-full transition-all duration-500"
+          sx={{
+            left: align === "left" ? "0" : `calc(-100% + ${size.width}px)`,
+          }}
+        >
+          {children}
+        </Box>
+        {size.width <= minWidth && align === "left" && (
+          <IconButton
+            className={`right-0 ${buttonStyle}`}
+            onClick={() => setAlign("right")}
+          >
+            <KeyboardArrowRight fontSize="medium" />
+          </IconButton>
+        )}
+      </Box>
+    </Box>
+  );
+}
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,41 +1,42 @@
 import { Box, Stack } from "@mui/material";
 import AdvertisementCard from "@/cards/AdvertisementCard/index";
 import AssetStatusCard from "@/cards/AssetStatusCard/index";
-import BotListCard from "@/cards/BotListCard/index";
 import BotTransactionCard from "@/cards/BotTransactionCard";
 import ChartCard from "@/cards/ChartCard";
 import CircleChartCard from "@/cards/CircleChartCard/index";
 import PresetsCard from "@/cards/PresetsCard";
 import ProfitRankCard from "@/cards/ProfitRankCard";
-import TestResultCard from "@/cards/TestResultCard";
 import Layout from "@/components/Layout";
+import Carousel from "@/components/Layout/Carousel/index";
 
 export default function Home() {
   return (
     <Layout>
-      <Stack className="gap-4">
-        <Stack direction="row" className="gap-4 h-[340px]">
-          <AssetStatusCard />
-          <CircleChartCard />
-          <AdvertisementCard />
+      <Carousel minWidth={1400}>
+        <Stack className="gap-4">
+          <Stack direction="row" className="gap-4 h-[340px]">
+            <AssetStatusCard />
+            <CircleChartCard />
+            <AdvertisementCard />
+          </Stack>
+          <Stack direction="row" className="gap-4 h-[436px]">
+            <Box className="w-7/12">
+              <ChartCard />
+            </Box>
+            <Box className="w-5/12">
+              <PresetsCard />
+            </Box>
+          </Stack>
+          <Stack direction="row" className="gap-4 h-[436px]">
+            <Box className="w-5/12">
+              <BotTransactionCard />
+            </Box>
+            <Box className="w-7/12">
+              <ProfitRankCard />
+            </Box>
+          </Stack>
         </Stack>
-        <Stack direction="row" className="gap-4 h-[436px]">
-          <Box className="w-7/12">
-            <ChartCard />
-          </Box>
-          <Box className="w-5/12">
-            <PresetsCard />
-          </Box>
-        </Stack>
-        <Stack direction="row" className="gap-4 h-[436px]">
-          <Box className="w-5/12">
-            <BotTransactionCard />
-          </Box>
-          <Box className="w-7/12">
-            <ProfitRankCard />
-          </Box>
-        </Stack>
-      </Stack>
+      </Carousel>
     </Layout>
   );
 }
