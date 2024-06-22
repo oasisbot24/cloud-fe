4567323b5072d384807c3bd3c3bf5b67cb92d36a
jeongwon0410 <wjddnjs2216@naver.com>
2024-06-22 18:53:49 +0900

    test

--- src/pages/oasisbot/index.tsx
+++ src/pages/oasisbot/index.tsx
@@ -1,10 +1,33 @@
-import TempCard from "@/cards/TempCard/index";
-import Layout from "@/components/Layout/index";
+import { Box, Stack } from "@mui/material";
+import BotListCard from "@/cards/BotListCard";
+// import OasisBotTransactionCard from "@/cards/OasisBotTransactionCard";
+import Layout from "@/components/Layout";
+import Carousel from "@/components/Layout/Carousel";
 
 function OasisBot() {
   return (
     <Layout>
-      <TempCard />
+      <Carousel minWidth={1600}>
+        <Stack direction="row" className="gap-4">
+          <Stack className="gap-4 w-8/12">
+            <Stack direction="row" className="gap-4 h-[434px]">
+              <Box className=" w-2/5">
+                <BotListCard />
+              </Box>
+              <Box className="w-3/5">
+                <BotListCard />
+              </Box>
+            </Stack>
+            <Stack direction="row" className="h-[460px]">
+              {/* <OasisBotTransactionCard /> */}
+              <BotListCard />
+            </Stack>
+          </Stack>
+          <Stack className="w-4/12">
+            <BotListCard />
+          </Stack>
+        </Stack>
+      </Carousel>
     </Layout>
   );
 }
