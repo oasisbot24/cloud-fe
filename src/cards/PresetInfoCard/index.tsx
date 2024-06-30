78192960da2371eb918c6a510207e569eda7f5ab
DeveloperRyou <DeveloperRyou@gmail.com>
2024-06-30 16:28:31 +0900

    FEAT: Preset Info 의 General Info 생성

--- src/cards/PresetInfoCard/index.tsx
+++ src/cards/PresetInfoCard/index.tsx
@@ -1,5 +1,33 @@
+import {
+  Avatar,
+  CardActions,
+  CardContent,
+  Stack,
+  Typography,
+} from "@mui/material";
 import Card from "@/cards/Card";
+import GeneralInfo from "@/cards/PresetInfoCard/GeneralInfo";
 
 export default function PresetInfoCard() {
-  return <Card>preset info</Card>;
+  return (
+    <Card>
+      <CardContent>
+        <Stack className="w-full items-center gap-8">
+          <Stack className="items-center gap-3">
+            <Avatar className="w-[108px] h-[108px]" />
+            <Typography variant="300B" className="text-font-1">
+              유저네임
+            </Typography>
+            <Typography variant="200R" className="text-font-3">
+              #PLATINUM #단타 #도전적투자자
+            </Typography>
+          </Stack>
+          <GeneralInfo type="exchange" />
+          <GeneralInfo type="balance" />
+          <GeneralInfo type="time" />
+        </Stack>
+      </CardContent>
+      <CardActions />
+    </Card>
+  );
 }
