8102709ba99fb395abd05e4ef1ca17a14946e2d7
DeveloperRyou <DeveloperRyou@gmail.com>
2024-06-30 14:12:19 +0900

    FIX: CustomTextField 수정

--- src/cards/OasisBotRunCard/index.tsx
+++ src/cards/OasisBotRunCard/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { CardActions, CardContent } from "@mui/material";
+import { CardActions, CardContent, Stack } from "@mui/material";
 import Card from "@/cards/Card";
 import CardHeader from "@/cards/CardHeader";
 import CustomTextField from "@/components/common/CustomTextField";
@@ -31,40 +31,36 @@ function OasisBotRunCard() {
         }
       />
       <CardContent>
-        <CustomTextField id="budget" title="주문가능 금액" variant="standard" />
-        <CustomTextField
-          id="transactionAmount"
-          title="거래금액을 입력 (최소 ₩5,000)"
-          variant="standard"
-        />
-        <OasisBotSelect
-          id="presets"
-          title="설정 프리셋"
-          variant="standard"
-          items={[
-            { label: "프리셋1", value: "preset1" },
-            { label: "프리셋2", value: "preset2" },
-          ]}
-          value={selectedPreset}
-          onChange={setSelectedPreset}
-        />
-        <OasisBotSelect
-          id="tradeItem"
-          title="매매종목"
-          variant="standard"
-          items={[
-            { label: "비트코인", value: "BTC" },
-            { label: "이더리움", value: "ETH" },
-          ]}
-          value={selectedTradeItem}
-          onChange={setSelectedTradeItem}
-        />
-        <CustomTextField
-          id="leverage"
-          title="레버리지를 입력해 주세요"
-          variant="standard"
-          mb="0"
-        />
+        <Stack className="gap-2">
+          <CustomTextField id="budget" label="주문가능 금액" />
+          <CustomTextField
+            id="transactionAmount"
+            label="거래금액을 입력 (최소 ₩5,000)"
+          />
+          <OasisBotSelect
+            id="presets"
+            title="설정 프리셋"
+            variant="standard"
+            items={[
+              { label: "프리셋1", value: "preset1" },
+              { label: "프리셋2", value: "preset2" },
+            ]}
+            value={selectedPreset}
+            onChange={setSelectedPreset}
+          />
+          <OasisBotSelect
+            id="tradeItem"
+            title="매매종목"
+            variant="standard"
+            items={[
+              { label: "비트코인", value: "BTC" },
+              { label: "이더리움", value: "ETH" },
+            ]}
+            value={selectedTradeItem}
+            onChange={setSelectedTradeItem}
+          />
+          <CustomTextField id="leverage" label="레버리지를 입력해 주세요" />
+        </Stack>
       </CardContent>
       <CardActions style={{ paddingLeft: 0, paddingRight: 0 }}>
         <OasisBotButton
--- src/components/common/CustomTextField.tsx
+++ src/components/common/CustomTextField.tsx
@@ -1,27 +1,35 @@
 import React from "react";
-import { InputLabel, TextField } from "@mui/material";
+import { InputLabel, Stack, TextField } from "@mui/material";
 
 type Props = {
   id: string;
-  title: string;
+  label?: string;
+  className?: string;
   variant?: "standard" | "outlined" | "filled";
-  mb?: string;
 };
 
-function CustomTextField({ id, title, variant, mb }: Props) {
+function CustomTextField({
+  id,
+  label,
+  variant = "standard",
+  className = "",
+}: Props) {
   return (
-    <div style={{ marginBottom: mb ?? "1rem" }}>
-      <InputLabel shrink htmlFor={id} size="normal">
-        {title}
-      </InputLabel>
+    <Stack className={className}>
+      {label && (
+        <InputLabel shrink htmlFor={id} size="normal">
+          {label}
+        </InputLabel>
+      )}
       <TextField
         fullWidth
         defaultValue=""
         id={id}
         variant={variant}
         size="small"
+        className="h-[30px] items-center justify-center"
       />
-    </div>
+    </Stack>
   );
 }
 
