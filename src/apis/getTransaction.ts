59ead02f7ed42e943b44760f6ebf7bca71bc5fb4
jeongwon0410 <wjddnjs2216@naver.com>
2024-06-30 16:15:45 +0900

    FEAT:transaction api

--- src/apis/getTransaction.ts
+++ src/apis/getTransaction.ts
@@ -2,13 +2,26 @@ import api from "@/apis/network";
 
 
 
-
-async function getTransaction(): Promise<[]> {
-  const res = await api.get<ApiResponseType<[]>>(
-      `/transaction?exchange=all`,
-  );
-  return res.data?.data;
+interface BotType {
+  coinName:string,
+  date:string,
+  exchane:string,
+  position:string,
+  presetName: string;
+  profitLoss: number;
+  profitLossRate:number;
+  startBalance: number;
+  totalPrice: number;
 }
 
+async function getTransaction(exchangeName: string): Promise<BotType[]> {
+  const res = await api.get<ApiResponseType<BotType[]>>(
+    `/transaction?exchange=${exchangeName}`,
+  );
+
+  return res.data?.data 
+}
+
+export type { BotType };
 export { getTransaction };
 
