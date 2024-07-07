b1d48db379af4e536064f94b625d1991d82ad00e
DeveloperRyou <DeveloperRyou@gmail.com>
2024-07-07 13:21:26 +0900

    FIX: Layout import 변경

--- src/pages/oasislab/index.tsx
+++ src/pages/oasislab/index.tsx
@@ -4,8 +4,8 @@ import RunOasisBotCard from "@/cards/oasisbot/OasisBotRunCard";
 import OasisLabInfoCard from "@/cards/oasislab/OasisLabInfoCard";
 import OasisLabResultChartCard from "@/cards/oasislab/OasisLabResultChartCard";
 import OasisLabResultInfoCard from "@/cards/oasislab/OasisLabResultInfoCard";
-import Layout from "@/layouts";
 import Carousel from "@/layouts/Carousel";
+import Layout from "@/layouts/Layout";
 
 export default function OasisLab() {
   return (
