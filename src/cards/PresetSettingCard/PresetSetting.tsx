5ce1ae0c2ee9439fb42a1975146b8e5e77d556a7
DeveloperRyou <DeveloperRyou@gmail.com>
2024-07-10 23:57:35 +0900

    FEAT: PresetSetting

--- src/cards/PresetSettingCard/PresetSetting.tsx
+++ src/cards/PresetSettingCard/PresetSetting.tsx
@@ -4,36 +4,25 @@ import FormSelect from "@/components/form/FormSelect";
 import FormTextField from "@/components/form/FormTextField";
 import presetAtom from "@/datas/preset";
 
-export default function PresetInfo() {
+export default function PresetSetting() {
   const [preset] = useAtom(presetAtom);
   return (
-    <Stack className="w-full items-center gap-4">
+    <Stack className="w-full items-center gap-2">
+      <FormSelect id="name" label="프리셋이름" items={[]} value={preset.name} />
       <FormSelect
-        id="name"
+        id="indicator"
         label="설정 보조지표"
         items={[]}
         value={preset.indicator}
-        disabled
       />
       <FormSelect
-        id="indicator"
+        id="position"
         label="진입 포지션"
         items={[]}
         value={preset.position}
-        disabled
       />
-      <FormTextField
-        id="profitRate"
-        label="익절률"
-        value={preset.profitRate}
-        disabled
-      />
-      <FormTextField
-        id="lossRate"
-        label="손절률"
-        value={preset.lossRate}
-        disabled
-      />
+      <FormTextField id="profitRate" label="익절률" value={preset.profitRate} />
+      <FormTextField id="lossRate" label="손절률" value={preset.lossRate} />
     </Stack>
   );
 }
