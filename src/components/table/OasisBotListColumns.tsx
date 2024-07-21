21aa0c8bdb14938a3afd012194317f892f6a380b
developerryou <developerryou@gmail.com>
2024-07-21 17:30:20 +0900

    FIX: columns로 테이블 열 정의 파일 수정

---
+++ src/components/table/OasisBotListColumns.tsx
@@ -0,0 +1,63 @@
+import { IconButton } from "@mui/material";
+import {
+  GridColDef,
+  GridRenderCellParams,
+  GridValidRowModel,
+} from "@mui/x-data-grid";
+import { BotType } from "@/apis/oasisbot/bot";
+import Icon from "../Icon";
+import CustomSwitch from "../common/CustomSwitch";
+
+const OasisBotListColumns: GridColDef[] = [
+  {
+    field: "presetName",
+    headerName: "프리셋 이름",
+    flex: 1,
+    headerClassName: "text-slate-500",
+    renderCell: (
+      params: GridRenderCellParams<GridValidRowModel, BotType["presetName"]>,
+    ) => (
+      <div className="flex items-center">
+        <div className="w-4/5 whitespace-normal">{params.value}</div>
+        <IconButton sx={{ width: "24px", height: "24px" }}>
+          <Icon src="/icons/basic/setting.png" size={24} />
+        </IconButton>
+      </div>
+    ),
+  },
+  {
+    field: "startBalance",
+    headerName: "실행 금액",
+    flex: 1,
+    headerClassName: "text-slate-500",
+  },
+  {
+    field: "runningTime",
+    headerName: "운영 기간",
+    flex: 1,
+    headerClassName: "text-slate-500",
+    renderCell: (
+      params: GridRenderCellParams<GridValidRowModel, BotType["runningTime"]>,
+    ) => <div className="whitespace-normal">{params.value}</div>,
+  },
+  {
+    field: "coinType",
+    headerName: "종목",
+    flex: 1,
+    headerClassName: "text-slate-500",
+    renderCell: (
+      params: GridRenderCellParams<GridValidRowModel, BotType["coinType"]>,
+    ) => <div className="whitespace-normal">{params.value}</div>,
+  },
+  {
+    field: "isRunning",
+    headerName: "상태",
+    flex: 0.5,
+    headerClassName: "text-slate-500",
+    renderCell: (
+      params: GridRenderCellParams<GridValidRowModel, BotType["isRunning"]>,
+    ) => <CustomSwitch defaultChecked={params.value} />,
+  },
+];
+
+export default OasisBotListColumns;
