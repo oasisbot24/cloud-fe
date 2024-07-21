5c24c5dd5ddcf0a7b131d2551eb361323350c5d3
developerryou <developerryou@gmail.com>
2024-07-21 17:49:29 +0900

    FIX: hook 에서 lib 함수로 변경

---
+++ src/libs/coin.ts
@@ -0,0 +1,14 @@
+const coinToIcon = (coin: string | undefined) => {
+  switch (coin) {
+    case "BTC":
+      return "/icons/crypto/btc-16.png";
+    case "ETH":
+      return "/icons/crypto/eth-16.png";
+    case "EOS":
+      return "/icons/crypto/eos-16.png";
+    default:
+      return "";
+  }
+};
+
+export { coinToIcon };
