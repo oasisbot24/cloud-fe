f665d4ef4431240ee837209c8a7fc52d121253e5
jeongwon0410 <wjddnjs2216@naver.com>
2024-08-04 21:12:10 +0900

    FEAT:ranking api and 실시간 거래내역 api

--- src/cards/oasisbot/OasisBotTransactionCard/BotTransaction.tsx
+++ src/cards/oasisbot/OasisBotTransactionCard/BotTransaction.tsx
@@ -1,15 +1,16 @@
 import { CardContent } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
-import useOasisBotTransactionColumns from "@/components/table/useOasisBotTransactionColumns";
+import OasisBotTransactionColumns from "@/components/table/OasisBotTransactionColumns";
+import { useBotInfo } from "@/hooks/query/useOasisBot";
 
 export default function BotTransaction() {
-  const { columns, OasisBotTransactionCompactRows, isLoading } =
-    useOasisBotTransactionColumns();
+  const { transactionQuery } = useBotInfo();
+  const { isLoading, data: OasisBotTransactionCompactRows } = transactionQuery;
   return (
     <CardContent sx={{ paddingTop: "0", maxHeight: "410px", overflow: "auto" }}>
       <DataGrid
         loading={isLoading}
-        columns={columns}
+        columns={OasisBotTransactionColumns}
         rows={OasisBotTransactionCompactRows ?? []}
         hideFooter
         sx={{ border: "none" }}
