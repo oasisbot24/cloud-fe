ef3554e1ceaa44ca3289944d8d2ceee7bc085873
Heeseung Koo <nrudev812@gmail.com>
2024-08-17 23:10:30 +0900

    FIX: 봇 실행시 거래소 지정

--- src/hooks/query/useOasisBot.ts
+++ src/hooks/query/useOasisBot.ts
@@ -1,15 +1,17 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { useAtom } from "jotai";
-import { botStart, botStop, getBot } from "@/apis/oasisbot/bot";
+import { useAtom, useAtomValue } from "jotai";
+import { BotStartType, botStart, botStop, getBot } from "@/apis/oasisbot/bot";
 import { getCoin } from "@/apis/oasisbot/coin";
 import { getTransaction } from "@/apis/oasisbot/getTransaction";
 import exchangeAtom from "@/datas/exchange";
 
 export function useBot() {
   const queryClient = useQueryClient();
+  const exchange = useAtomValue(exchangeAtom);
+
   const startBotMutation = useMutation({
-    mutationFn: botStart,
-    mutationKey: ["startBot"],
+    mutationFn: (data: BotStartType) => botStart(data, exchange),
+    mutationKey: ["startBot", exchange],
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["getBot"] });
     },
