e7e98f0916d48a3042f4b59342eeb51646dc78b3
developerryou <developerryou@gmail.com>
2024-08-20 14:32:06 +0900

    FIX: preset user info api 연결

--- src/cards/preset/PresetInfoCard/UserInfo.tsx
+++ src/cards/preset/PresetInfoCard/UserInfo.tsx
@@ -1,21 +1,32 @@
 import { Avatar, Stack, Typography } from "@mui/material";
+import { useAtom } from "jotai";
 import GeneralInfo from "@/cards/preset/PresetInfoCard/GeneralInfo";
+import authAtom from "@/datas/auth";
+import { useInfoTradeStyle } from "@/hooks/query/useInfo";
 
 export default function UserInfo() {
+  const [auth] = useAtom(authAtom);
+  const {
+    tradeStyleQuery: { data: tradeStyleData },
+  } = useInfoTradeStyle();
   return (
     <Stack className="w-full items-center gap-8">
       <Stack className="items-center gap-3">
-        <Avatar className="w-[108px] h-[108px]" />
+        <Avatar className="w-[108px] h-[108px]" src={auth.picture} />
         <Typography variant="300B" className="text-font-1">
-          유저네임
+          {auth?.name}
         </Typography>
         <Typography variant="200R" className="text-font-3">
-          #PLATINUM #단타 #도전적투자자
+          {tradeStyleData?.tag.map(keyword => (
+            <Typography key={keyword} variant="200R">
+              #{keyword}
+            </Typography>
+          ))}
         </Typography>
       </Stack>
       <GeneralInfo type="exchange" />
       <GeneralInfo type="balance" />
-      <GeneralInfo type="time" />
+      <GeneralInfo type="subscribe" />
     </Stack>
   );
 }
