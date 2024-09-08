590a1461313fdee54d8b0010b9345d55eb3c0aac
developerryou <developerryou@gmail.com>
2024-09-08 22:14:15 +0900

    FIX: import 순서 수정

--- src/hooks/query/useChart.ts
+++ src/hooks/query/useChart.ts
@@ -1,8 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import { useAtom } from "jotai";
 import { getCoinRatio } from "@/apis/chart/coinRatio";
-import exchangeAtom from "@/datas/exchange";
 import { getDashboardChart } from "@/apis/chart/dashboardChart";
+import exchangeAtom from "@/datas/exchange";
 
 export function useChart() {
   const [exchange] = useAtom(exchangeAtom);
@@ -12,7 +12,6 @@ export function useChart() {
   });
   return { coinRatioQuery };
 }
-
 
 export function useDashboardChart() {
   const dashboardChartQuery = useQuery({
