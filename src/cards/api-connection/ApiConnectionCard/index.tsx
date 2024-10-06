e677a182d7a19f5c6f60168d9caa33718682e279
developerryou <developerryou@gmail.com>
2024-10-06 21:05:03 +0900

    FIX: 진행중 디자인 개발

--- src/cards/api-connection/ApiConnectionCard/index.tsx
+++ src/cards/api-connection/ApiConnectionCard/index.tsx
@@ -4,7 +4,11 @@ import ExchangeSelectDescription from "@/components/signin/dialog/ExchangeSelect
 import { useUserExchangesQuery } from "@/hooks/query/useApiConnection";
 import ExchangeButton from "./ExchangeButton";
 
-export default function ApiConnectionCard() {
+export default function ApiConnectionCard({
+  processing,
+}: {
+  processing?: ExchangeType;
+}) {
   const {
     userExchangeQuery: { data: userExchanges },
   } = useUserExchangesQuery();
@@ -15,20 +19,24 @@ export default function ApiConnectionCard() {
           <ExchangeButton
             exchange="upbit"
             isConnected={userExchanges?.includes("upbit")}
+            isProcessing={processing === "upbit"}
           />
           <ExchangeButton
             exchange="okx"
             isConnected={userExchanges?.includes("okx")}
+            isProcessing={processing === "okx"}
           />
           <ExchangeButton
             exchange="binance"
             disabled
             isConnected={userExchanges?.includes("binance")}
+            isProcessing={processing === "binance"}
           />
           <ExchangeButton
             exchange="lbank"
             disabled
             isConnected={userExchanges?.includes("lbank")}
+            isProcessing={processing === "lbank"}
           />
         </Box>
         <ExchangeSelectDescription />
