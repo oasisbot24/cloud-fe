bdbb8c0cd12af9565234e75087efb9b6645a485b
Heeseung Koo <nrudev@naver.com>
2024-10-08 23:22:00 +0900

    REFACTOR: #94 1, 3번 리뷰 반영

--- src/cards/oasisbot/OasisBotTotalCard/index.tsx
+++ src/cards/oasisbot/OasisBotTotalCard/index.tsx
@@ -12,6 +12,8 @@ interface OasisBotTotalCardProps {
   value: string | number;
   unit: string;
   difference?: number;
+  arrow?: boolean;
+  mention?: string;
 }
 
 function OasisBotTotalCard({
@@ -22,6 +24,8 @@ function OasisBotTotalCard({
   value,
   unit,
   difference,
+  arrow,
+  mention,
 }: OasisBotTotalCardProps) {
   return (
     <Card>
@@ -38,14 +42,27 @@ function OasisBotTotalCard({
             <Chip
               label={
                 <Stack direction="row">
-                  <Image
-                    src="/icons/arrow/profitloss/up-small.png"
-                    alt="arrow"
-                    width={16}
-                    height={16}
-                  />
+                  {((difference === undefined && arrow) ||
+                    (difference !== undefined && difference >= 0)) && (
+                    <Image
+                      src="/icons/arrow/profitloss/up-small.png"
+                      alt="arrow"
+                      width={16}
+                      height={16}
+                    />
+                  )}
+                  {difference !== undefined && difference < 0 && (
+                    <Image
+                      src="/icons/arrow/profitloss/down-small.png"
+                      alt="arrow"
+                      width={16}
+                      height={16}
+                    />
+                  )}
                   <Typography fontSize={12} fontWeight={700}>
-                    전 날 보다 {difference}원 상승 했어요
+                    {mention}
+                    {difference !== undefined &&
+                      `전 날 보다 ${difference}원 ${difference >= 0 ? "상승" : "하락"} 했어요`}
                   </Typography>
                 </Stack>
               }
--- src/cards/oasisbot/OasisBotTotalCard/list.tsx
+++ src/cards/oasisbot/OasisBotTotalCard/list.tsx
@@ -20,6 +20,7 @@ function OasisBotTotalCardList() {
         value={botDataQuery.data?.totalTradePrice.value ?? 0}
         unit="원"
         difference={botDataQuery.data?.totalTradePrice.difference}
+        arrow
       />
       <OasisBotTotalCard
         id="bot-transaction"
@@ -29,22 +30,37 @@ function OasisBotTotalCardList() {
         value={botDataQuery.data?.totalTradeCount.value ?? 0}
         unit="회"
         difference={botDataQuery.data?.totalTradeCount.difference ?? 0}
+        arrow
       />
       <OasisBotTotalCard
         id="profit-rate"
         title="나의 봇 수익률 Top 1"
-        subtitle={`${exchangeToKorean(exchange)} 거래소의 ${botDataQuery.data?.maxProfitRate.presetName} - ${botDataQuery.data?.maxProfitRate.coinName}`}
+        subtitle={
+          !!botDataQuery.data?.maxProfitRate.presetName &&
+          !!botDataQuery.data?.maxProfitRate.coinName
+            ? `${exchangeToKorean(exchange)} 거래소의 ${botDataQuery.data?.maxProfitRate.presetName} - ${botDataQuery.data?.maxProfitRate.coinName}`
+            : "ㅤ"
+        }
         color="#F46565"
         value={botDataQuery.data?.maxProfitRate.rate ?? 0}
         unit="%"
+        arrow
+        mention="7일 동안 승률이 제일 높아요"
       />
       <OasisBotTotalCard
         id="winning-rate"
         title="나의 봇 승률 Top 1"
-        subtitle={`${exchangeToKorean(exchange)} 거래소의 ${botDataQuery.data?.maxWinRate.presetName} - ${botDataQuery.data?.maxWinRate.coinName}`}
+        subtitle={
+          !!botDataQuery.data?.maxWinRate.presetName &&
+          botDataQuery.data?.maxWinRate.coinName
+            ? `${exchangeToKorean(exchange)} 거래소의 ${botDataQuery.data?.maxWinRate.presetName} - ${botDataQuery.data?.maxWinRate.coinName}`
+            : "ㅤ"
+        }
         color="#223CE9"
         value={botDataQuery.data?.maxWinRate.rate ?? 0}
         unit="%"
+        arrow
+        mention="7일 동안 승률이 제일 높아요"
       />
       <OasisBotTotalCard
         id="copied-presets"
@@ -53,6 +69,7 @@ function OasisBotTotalCardList() {
         color="#2BB7F5"
         value=""
         unit=""
+        mention="OASIS 님의 운용자산"
       />
     </Stack>
   );
