1f52b605cbdf4ea543e8b823d754580c85ebfb3f
developerryou <developerryou@gmail.com>
2024-10-20 23:29:56 +0900

    FIX: exchange 로컬 스토리지 변경

--- src/cards/mypage/MypageCard/MypageExchange.tsx
+++ src/cards/mypage/MypageCard/MypageExchange.tsx
@@ -20,6 +20,7 @@ export default function MypageExchange() {
           <SigninDialog onClose={closeModal}>
             <ExchangeSelect
               onClick={(type: ExchangeType) => {
+                localStorage.setItem("exchange", type);
                 setExchange(type);
                 closeModal();
               }}
