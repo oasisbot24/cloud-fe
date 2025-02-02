a808a5228a852c9856e8e43c6a22928609a1e9ac
Heeseung Koo <nrudev@naver.com>
2025-02-02 19:30:15 +0900

    FIX: 깃헙 자동 빌드 오류 수정

--- src/components/dialog/BotValidationDialog.tsx
+++ src/components/dialog/BotValidationDialog.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-
 import { useRouter } from "next/router";
 
 import InfoDialog from "@/components/dialog/InfoDialog";
@@ -14,25 +12,21 @@ function BotValidationDialog({ code, message }: Props) {
   const { push } = useRouter();
   const { closeDialog } = useDialogGlobal();
 
-  useEffect(() => {
-    console.log(message);
-  }, []);
-  if (code === 8)
+  if (code === 8) {
     return <InfoDialog title="활성화 봇 제한" description={[message]} confirmFunc={closeDialog} />;
-  else {
-    return (
-      <InfoDialog
-        title="활성화 봇 제한"
-        description={[message]}
-        confirmFunc={() => {
-          closeDialog();
-          push("/subscribe");
-        }}
-        confirmBtnText="플랜 업그레이드"
-        cancellable
-      />
-    );
   }
+  return (
+    <InfoDialog
+      title="활성화 봇 제한"
+      description={[message]}
+      confirmFunc={() => {
+        closeDialog();
+        push("/subscribe");
+      }}
+      confirmBtnText="플랜 업그레이드"
+      cancellable
+    />
+  );
 }
 
 export default BotValidationDialog;
