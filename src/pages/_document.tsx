dd0b5b00f53df6d0a707f7804b4c21fb05670cc4
jeongwon0410 <wjddnjs2216@naver.com>
2025-02-26 13:53:51 +0900

    okx수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,15 +7,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      {process.env.NEXT_PUBLIC_MODE === "development" && (
+        <meta name="robots" content="noindex, nofollow" />
+      )}
       <link
         rel="stylesheet prefetch"
         href="https://gist.githubusercontent.com/mfd/614e2e80a22b878bde63cb471cbe677e/raw/eef2aabfd0d588d43285431186a0cf70305ceb80/sanfrancisco-font.css"
       />
       <script src="https://static.okx.com/cdn/assets/okfe/libs/okxOAuth/index.js" async />
-
-      {process.env.NEXT_PUBLIC_MODE === "development" && (
-        <meta name="robots" content="noindex, nofollow" />
-      )}
 
       <body>
         <Main />
