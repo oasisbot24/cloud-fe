4f4536ea2d932bb55de03e19a9006debaafa5c9a
seo <seo@seoui-MacBookAir.local>
2025-05-11 21:30:17 +0900

    FEAT:support card

--- src/cards/mypage/MypageCard/index.tsx
+++ src/cards/mypage/MypageCard/index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import { useRouter } from "next/router";
 
-import { Button, IconButton, InputBase, Stack, Typography } from "@mui/material";
+import { InputBase, Stack, Typography } from "@mui/material";
 import { useQueryClient } from "@tanstack/react-query";
 import { useAtom } from "jotai";
 
